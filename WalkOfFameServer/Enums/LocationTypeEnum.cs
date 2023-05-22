1458c9ec4be616b2bcb1f31e4f8881c5df9c8cc8
wellbritto98 <eng.jwellbritto@gmail.com>
2023-05-22 11:09:35 -0300

    Criação dos Enums LocationType e ZoneType

--- WalkOfFameServer/Enums/LocationTypeEnum.cs
+++ WalkOfFameServer/Enums/LocationTypeEnum.cs
@@ -2,6 +2,53 @@
 {
     public enum LocationTypeEnum
     {
+        Airport,
+        Bank,
+        BusTerminal,
+        CarDealer,
+        CityHall,
+        ConstructionCompany,
+        Countryside,
+        CourtHouse,
+        Factory,
+        FestivalHall, //Espaço onde acontece cerimônia de oscar, cannes, etc.
+        FireStation, 
+        GarbageDump,
+        Graveyard,
+        Gym,
+        HeadOffice,
+        Highway,
+        Hospital,
+        Hotel,
+        JamPlace, //Local de studio de ensaio
+        LostAndFound,
+        Museum,
+        Nursery,
+        Park,
+        ParkingGarage,
+        PetShop,
+        PlasticSurgeryCenter,
+        PoliceStation,
+        PowerPlant,
+        Prison,
+        PsychologyClinic,
+        PublicBeach,
+        RealEstateAgency,
+        RecordStudio, //studio de gravação que deve ser alterado para estudio tipo DISNEY
+        Restaurant,
+        School,
+        SecurityFirm, //Empresa de segurança particular
+        Shop,
+        SocialClub,
+        Stadium,
+        StageEquipmentShop,
+        SubwayLine,
+        TattooParlor,
+        Temple,
+        TVStation,
+        University,
+        WaterReservoir 
+
         
     }
 }
