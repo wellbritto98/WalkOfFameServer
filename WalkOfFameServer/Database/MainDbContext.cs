d782fa5f8f976feacb004da6ad250d3833d62703
MyNameIsBatman <montevechiol@gmail.com>
2023-05-25 23:46:19 -0300

    Add ArticleSeeder to Context

--- WalkOfFameServer/Database/MainDbContext.cs
+++ WalkOfFameServer/Database/MainDbContext.cs
@@ -30,6 +30,7 @@ namespace WalkOfFameServer.Database
         {
             base.OnModelCreating(modelBuilder);
             new CitySeeder(modelBuilder, IdGenerator).Seed();
+            new ArticleSeeder(modelBuilder, IdGenerator).Seed();
         }
         
         public DbSet<User> Users { get; set; }
